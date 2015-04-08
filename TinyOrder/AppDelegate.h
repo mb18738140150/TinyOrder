424abee3d0d10adc582d3911c082010ef605292e
仙林 <xianlin@xianlindeiMac.local>
2015-04-08 11:20:18 +0800

    Initial Commit

---
+++ TinyOrder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TinyOrder
+//
+//  Created by 仙林 on 15/4/8.
+//  Copyright (c) 2015年 仙林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TinyOrderTests/TinyOrderTests.m
@@ -0,0 +1,40 @@
+//
+//  TinyOrderTests.m
+//  TinyOrderTests
+//
+//  Created by 仙林 on 15/4/8.
+//  Copyright (c) 2015年 仙林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TinyOrderTests : XCTestCase
+
+@end
+
+@implementation TinyOrderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
